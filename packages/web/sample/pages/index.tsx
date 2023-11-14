fb957eb9025ff302de9b79d88a341e24d687d1a2
park <parkkinam@vitruv.co.kr>
2023-11-14 13:42:21 +0900

    3

---
+++ packages/web/sample/pages/index.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Button } from "@mts5/share";
+
+export default function Home() {
+  return <Button>기남</Button>;
+}
